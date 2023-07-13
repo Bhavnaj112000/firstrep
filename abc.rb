f548f8fbaa1fe0e8e2f6b01438bfaff07318a9da
Bhavna <bhavna@gmail.com>
2023-07-13 15:47:50 +0530

    updated

---
+++ abc.rb
@@ -0,0 +1,10 @@
+# Initialising an array and calling each_with_index function
+[5, 10, 15, 20, 25, 30].each_with_index do |num, idx|
+
+# Getting the values of the array
+  puts "#{num}"
+  if ((idx) % 2 == 0)
+    puts "end of line"
+  end
+end
+puts "Bhavna"
