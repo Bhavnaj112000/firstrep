9670b8843fe27106b7d4c1a09d9e8a82146424f3
Bhavnaj112000 <139379421+Bhavnaj112000@users.noreply.github.com>
2023-07-17 23:27:48 +0530

    flow control- calculator
    **Flow Control - Calculator:** Create a Calculator class with methods for addition, subtraction, multiplication, and division. Implement flow control to allow the user to choose the operation and input the numbers. Display the result to the user.

---
+++ flow_control.rb
@@ -0,0 +1,39 @@
+# **Flow Control - Calculator:** Create a Calculator class with methods for addition, subtraction, multiplication, 
+# and division. Implement flow control to allow the user to choose the operation and input the numbers. Display the result to the user.
+
+class Calculator
+     attr_accessor :a, :b
+
+def initialize(a,b)
+  @a=a 
+  @b=b
+
+end
+
+def add
+  a + b
+end
+
+def substract
+  a - b
+end
+
+def multiply
+  a * b
+end
+
+
+
+def divide
+  a / b
+end
+
+
+end
+
+calc = Calculator.new(6,4)
+p "sum = #{calc.add}"
+p "substraction = #{calc.substract}"
+p "multiplication = #{calc.multiply}"
+p "Divide reult = #{calc.divide}"
+
